0ab5fb6fed2e0e6e540b69250537f663c3d7ad79
Thomas Otim <totim1992@gmail.com>
2020-12-16 09:27:20 +0300

    Welcome screen layout

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  Button,
+} from "react-native";
+import {MaterialCommunityIcons} from '@expo/vector-icons'
+
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WelcomeScreen />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {StyleSheet, ImageBackground, View, Image} from "react-native";
+
+function WelcomeScreen() {
+  return (
+    <ImageBackground
+      blurRadius={2}
+      style={styles.background}
+      source={require("../assets/background.jpg")}>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    position: "absolute",
+    top: 70,
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+});
+
+export default WelcomeScreen;
