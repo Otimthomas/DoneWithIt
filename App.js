fc1151a80d45754e8e91e6bd1292f3cf52e7193d
Thomas Otim <totim1992@gmail.com>
2020-12-17 12:28:21 +0300

    ViewImageScreen layout

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
-import {StatusBar} from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  View,
-  Image,
-  TouchableWithoutFeedback,
-  Button,
-} from "react-native";
-import {MaterialCommunityIcons} from '@expo/vector-icons'
+import {StyleSheet} from "react-native";
 
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  
   return (
-    <WelcomeScreen />
+    // <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
 
