cd58890f847a1a4f995ceeab12306467810ba45e
Thomas Otim <totim1992@gmail.com>
2020-12-28 11:55:21 +0300

    created a Picker from scratch without using third-party liabrary

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -1,8 +1,7 @@
 import React from "react";
-import {View, StyleSheet, TextInput, Platform} from "react-native";
+import {View, StyleSheet, TextInput} from "react-native";
 import {MaterialCommunityIcons} from "@expo/vector-icons";
 
-import colors from "../config/colors";
 import defaultStyles from "../config/styles";
 
 export default function AppTextInput({icon, ...otherProps}) {
@@ -11,9 +10,9 @@ export default function AppTextInput({icon, ...otherProps}) {
     <View style={styles.container}>
       {icon && (
         <MaterialCommunityIcons
-          name='email'
+          name={icon}
           size={25}
-          color={colors.medium}
+          color={defaultStyles.colors.medium}
           style={styles.icon}
         />
       )}
@@ -30,7 +29,7 @@ export default function AppTextInput({icon, ...otherProps}) {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    backgroundColor: colors.light,
+    backgroundColor: defaultStyles.colors.light,
     padding: 15,
     borderRadius: 25,
     width: "100%",
